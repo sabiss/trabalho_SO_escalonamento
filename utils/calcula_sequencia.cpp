da2fefd61268d0251446cdf234fccb351982952d
Sabrina Bezerra <sabrinabezsilva@gmail.com>
2026-08-26 18:26:39 -0300

    FEAT: calcula a sequência e atualiza as variáveis de dados

---
+++ utils/calcula_sequencia.cpp
@@ -0,0 +1,31 @@
+#include <utility>
+#include "calcula_sequencia.h"
+
+using ull = unsigned long long;
+
+void calcula_sequencia(const ull& inicio_intervalo, const ull& fim_intervalo, std::pair<ull, ull>& recorde, ull QTD, const ull& limiar){
+    for(ull numero_do_intervalo = inicio_intervalo; numero_do_intervalo <= fim_intervalo; numero_do_intervalo++){
+        ull numero = numero_do_intervalo;
+        ull passos = 0;
+
+        while(numero != 1){
+            bool numero_eh_par = numero%2==0;
+            passos++;
+
+            if(numero_eh_par){
+                numero = numero/2;
+            }else{
+                numero = 3*numero + 1;
+            }
+        }
+
+        bool recorde_passos_ultrapassado = recorde.first < passos;
+        if(recorde_passos_ultrapassado){
+          recorde.first = passos;
+          recorde.second = numero;  
+        }
+
+        bool precisou_mais_passos_que_o_limiar = passos > limiar;
+        if(precisou_mais_passos_que_o_limiar)QTD++;
+    }
+}
---
+++ utils/calcula_sequencia.h
@@ -0,0 +1,5 @@
+#include <utility>
+
+using ull = unsigned long long;
+
+void calcula_sequencia(const ull& inicio_intervalo, const ull& fim_intervalo, std::pair<ull, ull>& recorde, ull QTD, const ull& limiar);
