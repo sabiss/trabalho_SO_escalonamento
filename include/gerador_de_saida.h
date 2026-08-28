3ab76ee083f2712d621b044a6f48c56fc0b53ffd
Sabrina Bezerra <sabrinabezsilva@gmail.com>
2026-08-28 16:46:33 -0300

    FIX: atualizando parâmetros

--- include/gerador_de_saida.h
+++ include/gerador_de_saida.h
@@ -5,6 +5,6 @@
 
 using ull = unsigned long long;
 
-void gerar_arquivo_saida(std::string arquivo_saida, const std::string& modo, const std::string& particao, const int& W, const ull& TAMANHO_INTERVALO, const double& tempo_total, const ull& QTD, const std::pair<ull,ull>& recorde);
+void gerar_arquivo_saida(std::string arquivo_saida, const std::string& modo, const std::string& particao, const int& W, const ull& TAMANHO_INTERVALO, const double& tempo_total, const double& tempo_max_worker, const double& tempo_min_worker, const double& tempo_agregacao, const ull& QTD, const std::pair<ull,ull>& recorde);
 
 #endif
