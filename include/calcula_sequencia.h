c1956aa48569db77e3ec6d71689d98f2e6ef150a
Sabrina Bezerra <sabrinabezsilva@gmail.com>
2026-08-29 09:35:13 -0300

    FIX: concertando ordem dos parâmetros

--- include/calcula_sequencia.h
+++ include/calcula_sequencia.h
@@ -2,4 +2,4 @@
 
 using ull = unsigned long long;
 
-void calcula_sequencia(const ull& inicio_intervalo, const ull& fim_intervalo, std::pair<ull, ull>& recorde, ull QTD, const ull& limiar, double& tempo_total);
+void calcula_sequencia(const ull& inicio_intervalo, const ull& fim_intervalo, std::pair<ull, ull>& recorde, ull& QTD, const ull& limiar, double& tempo_total);
