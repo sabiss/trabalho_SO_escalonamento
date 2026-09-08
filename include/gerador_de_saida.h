c9c5a05843e7d9252d775e9594c7a7aa5c6018fe
Sabrina Bezerra <sabrinabezsilva@gmail.com>
2026-09-08 20:45:48 -0300

    REFACTOR: indentação e alteração dos tipos dos parâmetros modo e partição

--- include/gerador_de_saida.h
+++ include/gerador_de_saida.h
@@ -1,10 +1,20 @@
 #ifndef GERADOR_DE_SAIDA_H
 #define GERADOR_DE_SAIDA_H
 
-#include <string>
+#include "tipos.h"
 
-using ull = unsigned long long;
-
-void gerar_arquivo_saida(std::string arquivo_saida, const int& modo, const std::string& particao, const int& W, const ull& TAMANHO_INTERVALO, const double& tempo_total, const double& tempo_max_worker, const double& tempo_min_worker, const double& tempo_agregacao, const ull& QTD, const ull& recorde_numero, const ull& recorde_saltos);
+void gerar_arquivo_saida(
+    std::string arquivo_saida, 
+    const ModoOperacao& modo, 
+    const TipoParticao& particao, 
+    const int& W, 
+    const ull& TAMANHO_INTERVALO, 
+    const double& tempo_total, 
+    const double& tempo_max_worker, 
+    const double& tempo_min_worker, 
+    const double& tempo_agregacao, 
+    const ull& QTD, 
+    const ull& recorde_numero, 
+    const ull& recorde_saltos);
 
 #endif
