e73e0c9260c9b6a126db13ae2222c4b454b338c7
Sabrina Bezerra <sabrinabezsilva@gmail.com>
2026-09-08 18:24:53 -0300

    FIX: corrigindo tipo do parãmetro modo de string para init

--- include/gerador_de_saida.h
+++ include/gerador_de_saida.h
@@ -3,6 +3,6 @@
 
 using ull = unsigned long long;
 
-void gerar_arquivo_saida(std::string arquivo_saida, const std::string& modo, const std::string& particao, const int& W, const ull& TAMANHO_INTERVALO, const double& tempo_total, const double& tempo_max_worker, const double& tempo_min_worker, const double& tempo_agregacao, const ull& QTD, const ull& recorde_numero, const ull& recorde_saltos);
+void gerar_arquivo_saida(std::string arquivo_saida, const int& modo, const std::string& particao, const int& W, const ull& TAMANHO_INTERVALO, const double& tempo_total, const double& tempo_max_worker, const double& tempo_min_worker, const double& tempo_agregacao, const ull& QTD, const ull& recorde_numero, const ull& recorde_saltos);
 
 #endif
