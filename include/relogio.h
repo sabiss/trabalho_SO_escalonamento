28ea1785971a9627efe029d2b849a3c34b13100d
Sabrina Bezerra <sabrinabezsilva@gmail.com>
2026-09-08 18:24:22 -0300

    REFACTOR: melhorando o nome dos métodos para fazerem mais sentido ao serem reutilizados

--- include/relogio.h
+++ include/relogio.h
@@ -8,8 +8,8 @@ private:
     std::chrono::high_resolution_clock::time_point tempo_fim_sequencia;
 
 public:
-    void iniciar_marcacao_sequencia();
-    void parar_marcacao_sequencia();
+    void iniciar_marcacao();
+    void parar_marcacao();
     double calcular_tempo_total();
 };
 
--- src/relogio.cpp
+++ src/relogio.cpp
@@ -1,10 +1,10 @@
 #include "../include/relogio.h"
 
-void Relogio::iniciar_marcacao_sequencia() {
+void Relogio::iniciar_marcacao() {
     tempo_inicio_sequencia = std::chrono::high_resolution_clock::now();
 }
 
-void Relogio::parar_marcacao_sequencia() {
+void Relogio::parar_marcacao() {
     tempo_fim_sequencia = std::chrono::high_resolution_clock::now();
 }
 
