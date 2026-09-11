2be1ec1268a9f3a945f9f22ca2bda3a2456d4ed0
Sabrina Bezerra <sabrinabezsilva@gmail.com>
2026-09-11 08:48:19 -0300

    REFACTOR: renomeando arquivo, função e melhorando legibilidade dos parâmetros

--- include/calcula_intervalo_bloco.h
+++ include/calcula_intervalo_bloco.h
@@ -5,6 +5,10 @@
 
 using ull = unsigned long long;
 
-std::pair<ull, ull> calcula_intervalo(const ull& inicio_intervalo_global, const ull& fim_intervalo_global, const ull tamanho_intervalo, const int& W, const int& id_worker);
+std::pair<ull, ull> calcula_intervalo_bloco(
+    const ull& inicio_intervalo_global, 
+    const ull& fim_intervalo_global, 
+    const int& W, 
+    const int& id_worker);
 
 #endif
--- src/calcula_intervalo_bloco.cpp
+++ src/calcula_intervalo_bloco.cpp
@@ -1,9 +1,17 @@
 #include <utility>
+#include "../include/calcula_intervalo_bloco.h"
 
 using ull = unsigned long long;
 
-std::pair<ull, ull> calcula_intervalo(const ull& inicio_intervalo_global, const ull& fim_intervalo_global, const ull tamanho_intervalo, const int& W, const int& id_worker) {
-    ull tamanho_fatia = tamanho_intervalo / W;
+std::pair<ull, ull> calcula_intervalo_bloco(
+    const ull& inicio_intervalo_global, 
+    const ull& fim_intervalo_global, 
+    const int& W, 
+    const int& id_worker
+) {
+    const ull tamanho_intervalo_global = fim_intervalo_global - inicio_intervalo_global + 1;
+    
+    ull tamanho_fatia = tamanho_intervalo_global / W;
     ull inicio_intervalo_worker = inicio_intervalo_global + (id_worker * tamanho_fatia);
     //se caso tiver resto, o último worker é quem vai pegar ele pra dividir
     ull fim_intervalo_worker = (id_worker == W - 1) ? fim_intervalo_global : inicio_intervalo_worker + tamanho_fatia - 1;
